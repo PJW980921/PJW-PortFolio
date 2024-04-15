77e36df63086402687849032ab4662268119105c
박지원 <jackgg12322@gmail.com>
2024-04-15 20:23:55 +0900

    setting : SEO 초기 셋팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App';
+import { hydrate, render } from 'react-dom';
+const rootElement = document.getElementById('root');
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+if (rootElement?.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
