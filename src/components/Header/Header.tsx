acfaa6fb971438df874dd43715fe7b3ca1f6dc6d
박지원 <jackgg12322@gmail.com>
2024-04-15 21:04:01 +0900

    setting : sass install & components 생성

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="/">PJW`s Portfolio</a>
+        <ul>
+          <li>
+            <a href="#">About Me</a>
+          </li>
+          <li>
+            <a href="#">Skills</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,13 @@
+import AboutMe from './AboutMe/AboutMe';
+import Projects from './Projects/Projects';
+import Skills from './Skills/Skills';
+
+export default function Main() {
+  return (
+    <main>
+      <AboutMe />
+      <Skills />
+      <Projects />
+    </main>
+  );
+}
