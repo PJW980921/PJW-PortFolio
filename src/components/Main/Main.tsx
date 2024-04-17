e29e49e4aee6768c5ff917a91357144a03a12111
박지원 <jackgg12322@gmail.com>
2024-04-17 20:26:43 +0900

    style : main style 추가

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,11 @@
 import AboutMe from './AboutMe/AboutMe';
 import Projects from './Projects/Projects';
 import Skills from './Skills/Skills';
+import '../Main/Main.scss';
 
 export default function Main() {
   return (
-    <main>
+    <main className="main-container">
       <AboutMe />
       <Skills />
       <Projects />
