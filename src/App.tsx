dba5e717dad5e36571f8a0989049163d163f90f8
박지원 <jackgg12322@gmail.com>
2024-04-18 15:12:11 +0900

    fix : 로컬스토리지 값이 비었을 때 data-theme 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import Main from './components/Main/Main';
 
 function App() {
   useEffect(() => {
+    if (document.documentElement.getAttribute('data-theme')) {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('data-theme', 'light');
+    } else {
+      localStorage.getItem('data-theme');
+    }
     const getSaveDarkModeData = localStorage.getItem('data-theme') as string;
     document.documentElement.setAttribute('data-theme', getSaveDarkModeData);
   }, []);
