1c7a98ce853505e3528c777a7c249c971ceb69a3
박지원 <jackgg12322@gmail.com>
2024-04-19 20:36:37 +0900

    feat : Project UI 구현중

--- src/components/Main/Projects/Projects.tsx
+++ src/components/Main/Projects/Projects.tsx
@@ -1,4 +1,86 @@
+import { useEffect, useState } from 'react';
 import '../Projects/Projects.scss';
+import { LuFolderSearch2 } from 'react-icons/lu';
+import { FaGithubSquare } from 'react-icons/fa';
+import { FaLink } from 'react-icons/fa';
+interface ProjectsProps {
+  id: number;
+  image: string;
+  title: string;
+  description: string;
+  stacks: string;
+  github: string;
+  site: string;
+}
 export default function Projects() {
-  return <section id="Projects" className="projects-container"></section>;
+  const [projectList, setProjectList] = useState<ProjectsProps[]>([]);
+
+  useEffect(() => {
+    fetch('/data/Projects.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setProjectList(data);
+      });
+  }, []);
+
+  return (
+    <section id="Projects" className="projects-container">
+      <h1 id="Projects" className="projects-title">
+        <LuFolderSearch2 />
+        Projects
+      </h1>
+      <article className="projects-article">
+        {projectList.map((item) => {
+          return (
+            <div className="projects-box" key={item.id}>
+              <div className="projects-box_img_box">
+                <h1 className="projects-box_title">{item.title}</h1>
+                <img
+                  src={item.image}
+                  alt="프로젝트 이미지"
+                  className="projects-box_img"
+                  width={200}
+                  height={200}
+                />
+              </div>
+              <div className="projects-description_container">
+                <div className="projects-box_description_box">
+                  <p className="projects-box_description">{item.description}</p>
+                </div>
+                <div className="projects-box_stacks_box">
+                  <p className="projects-box_stacks">
+                    주요 기술 스택 : {item.stacks}
+                  </p>
+                </div>
+                <div className="projects-links">
+                  <div className="projects-box_github_box">
+                    <a
+                      href={item.github}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="projects-box_github"
+                      aria-label="Project GitHub"
+                    >
+                      <FaGithubSquare />
+                    </a>
+                  </div>
+                  <div className="projects-box_site_box">
+                    <a
+                      href={item.site}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="projects-box_site"
+                      aria-label="Project Link"
+                    >
+                      <FaLink />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </article>
+    </section>
+  );
 }
