c926b4926198603c92cdb934abffeffaab80d6dd
박지원 <jackgg12322@gmail.com>
2024-04-19 15:09:10 +0900

    feat : 라이브러리 및 hydrateRoot 함수를 활용하여 SEO 최적화

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
-import { ReactNode } from 'react';
 import App from './App';
-import { hydrateRoot } from 'react-dom/client';
-const domNode = document.getElementById('root') as HTMLElement;
-const root = hydrateRoot(domNode, document.getElementById('root') as ReactNode);
+import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 
-if (domNode?.hasChildNodes()) {
-  hydrateRoot(domNode, <App />);
+const container = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(container);
+
+if (container.hasChildNodes()) {
+  ReactDOM.hydrateRoot(
+    container,
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  );
 } else {
-  root.render(<App />);
+  root.render(
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  );
 }
