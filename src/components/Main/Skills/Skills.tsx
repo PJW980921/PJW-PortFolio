6b95ea228f92d4b324cacba424c482bf30d8cd3c
박지원 <jackgg12322@gmail.com>
2024-04-19 10:02:38 +0900

    refactor : Skills 목업데이터 생성 및 컴포넌트 코드 수정

--- src/components/Main/Skills/Skills.tsx
+++ src/components/Main/Skills/Skills.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react';
 import '../Skills/Skills.scss';
 import { IoHammerOutline } from 'react-icons/io5';
+interface SkillsProps {
+  title: string;
+  image: string;
+  alt: string;
+}
 
 export default function Skills() {
+  const [skillList, setSkillList] = useState<SkillsProps[]>([]);
+
+  useEffect(() => {
+    fetch('/data/Skills.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setSkillList(data);
+      });
+  });
   return (
     <section>
       <h1 id="Skills" className="skills-title">
@@ -10,62 +25,14 @@ export default function Skills() {
       </h1>
       <div className="skills-container">
         <div className="front-end_container">
-          <div className="front-end_box">
-            <h1 className="front-end_title">Publishing</h1>
-            <img
-              src="image/publish-img.png"
-              className="skills-img"
-              alt="프론트엔드 기술 스택 이미지"
-            />
-          </div>
-          <div className="front-end_box">
-            <h1 className="front-end_title">FrameWorks</h1>
-            <img
-              src="image/frameWorks-img.png"
-              className="skills-img"
-              alt="프론트엔드 기술 스택 이미지"
-            />
-          </div>
-          <div className="front-end_box">
-            <h1 className="front-end_title">Styles</h1>
-            <img
-              src="image/styles-img.png"
-              className="skills-img"
-              alt="프론트엔드 기술 스택 이미지"
-            />
-          </div>
-          <div className="front-end_box">
-            <h1 className="front-end_title">Libraries</h1>
-            <img
-              src="image/libs-img.png"
-              className="skills-img"
-              alt="프론트엔드 기술 스택 이미지"
-            />
-          </div>
-          <div className="front-end_box">
-            <h1 className="front-end_title">Lints</h1>
-            <img
-              src="image/lints-img.png"
-              className="skills-img"
-              alt="프론트엔드  이미지"
-            />
-          </div>
-          <div className="front-end_box">
-            <h1 className="front-end_title">Version Control</h1>
-            <img
-              src="image/version-control-img.png"
-              className="skills-img"
-              alt="프론트엔드 버전컨트롤 이미지"
-            />
-          </div>
-          <div className="front-end_box">
-            <h1 className="front-end_title">Communication</h1>
-            <img
-              src="image/communication-img.png"
-              className="skills-img"
-              alt="프론트엔드 커뮤니케이션 이미지"
-            />
-          </div>
+          {skillList.map((item) => {
+            return (
+              <div className="front-end_box">
+                <h1 className="front-end_title">{item.title}</h1>
+                <img src={item.image} className="skills-img" alt={item.alt} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
